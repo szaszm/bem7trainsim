04328de44b25b04797c14f14972c16af413ffe41
Csutorás Robin <csutorasr@hotmail.com>
2017-05-08 00:28:23 +0200

    credits view

--- src/bem7trainsim/CreditsView.java
+++ src/bem7trainsim/CreditsView.java
@@ -7,7 +7,7 @@ import java.awt.event.MouseEvent;
  * Created by Kristóf on 2017. 05. 07..
  */
 public class CreditsView extends View {
-
+    static int lineHeight = 20;
 
     public CreditsView() {
     }
@@ -19,6 +19,11 @@ public class CreditsView extends View {
 
     @Override
     void draw(Graphics g) {
-        g.drawString("Csutorás Robin\nGnandt Balázs\nSzász Márton\nTamás Csongor\nZabó Kristóf",g.getClipBounds().width / 2, g.getClipBounds().height / 2);
+        g.drawString("Csutorás Robin",g.getClipBounds().width / 2 - 45, g.getClipBounds().height / 2 - lineHeight * 2);
+        g.drawString("Gnandt Balázs",g.getClipBounds().width / 2 - 40, g.getClipBounds().height / 2 - lineHeight);
+        g.drawString("Szász Márton",g.getClipBounds().width / 2 - 35, g.getClipBounds().height / 2);
+        g.drawString("Tamás Csongor",g.getClipBounds().width / 2 - 45, g.getClipBounds().height / 2 + lineHeight);
+        g.drawString("Zabó Kristóf",g.getClipBounds().width / 2 - 32, g.getClipBounds().height / 2 + lineHeight * 2);
+        g.drawString("Back",g.getClipBounds().width / 2 - 12, g.getClipBounds().height / 2 + lineHeight * 3);
     }
 }
