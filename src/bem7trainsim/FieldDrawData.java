fdc96c578e0acc19110b74e17722c618b7ce0310
Csutorás Robin <csutorasr@hotmail.com>
2017-05-08 01:43:56 +0200

    images started

--- src/bem7trainsim/FieldDrawData.java
+++ src/bem7trainsim/FieldDrawData.java
@@ -6,6 +6,7 @@ import java.awt.image.BufferedImage;
 import java.awt.image.RescaleOp;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 import static java.awt.image.BufferedImage.TYPE_CUSTOM;
@@ -36,63 +37,62 @@ public class FieldDrawData {
         }
     }
 
-    static private BufferedImage straightHorizontal;
-    static private BufferedImage straightVertical;
-    static private BufferedImage curveNE;
-    static private BufferedImage curveNW;
-    static private BufferedImage curveSE;
-    static private BufferedImage curveSW;
-    static private BufferedImage cross;
-    static private BufferedImage grass1;
-    static private BufferedImage grass2;
-    static private BufferedImage downStationHorizontalBlue;
-    static private BufferedImage downStationHorizontalGreen;
-    static private BufferedImage downStationHorizontalRed;
-    static private BufferedImage downStationHorizontalYellow;
-    static private BufferedImage downStationVerticallBlue;
-    static private BufferedImage downStationVerticalGreen;
-    static private BufferedImage downStationVerticalRed;
-    static private BufferedImage downStationVerticalYellow;
-    static private BufferedImage upStationHorizontalBlue;
-    static private BufferedImage upStationHorizontalGreen;
-    static private BufferedImage upStationHorizontalRed;
-    static private BufferedImage upStationHorizontalYellow;
-    static private BufferedImage upStationVerticallBlue;
-    static private BufferedImage upStationVerticalGreen;
-    static private BufferedImage upStationVerticalRed;
-    static private BufferedImage upStationVerticalYellow;
-    static private BufferedImage switchNorthRightCurve;
-    static private BufferedImage switchNorthRightStraight;
-    static private BufferedImage switchNorthLeftCurve;
-    static private BufferedImage switchNorthLeftStraight;
-    static private BufferedImage switchSouthRightCurve;
-    static private BufferedImage switchSouthRightStraight;
-    static private BufferedImage switchSouthLeftCurve;
-    static private BufferedImage switchSouthLeftStraight;
-    static private BufferedImage switchWestRightCurve;
-    static private BufferedImage switchWestRightStraight;
-    static private BufferedImage switchWestLeftCurve;
-    static private BufferedImage switchWestLeftStraight;
-    static private BufferedImage switchEastRightCurve;
-    static private BufferedImage switchEastRightStraight;
-    static private BufferedImage switchEastLeftCurve;
-    static private BufferedImage switchEastLeftStraight;
-
-
+    static HashMap<String, BufferedImage> images;
 
     static {
         try {
-            valto = ImageIO.read(new File("random.png"));
+            String path = "Piskels/";
+            images = new HashMap<>();
+            images.put("straightHorizontal",ImageIO.read(new File(path + "/straightHorizontal.png")));
+            images.put("straightVertical",ImageIO.read(new File(path + "/straightVertical.png")));
+            images.put("curveNE",ImageIO.read(new File(path + "/curveNE.png")));
+            images.put("curveNW",ImageIO.read(new File(path + "/curveNW.png")));
+            images.put("curveSE",ImageIO.read(new File(path + "/curveSE.png")));
+            images.put("curveSW",ImageIO.read(new File(path + "/curveSW.png")));
+            images.put("cross",ImageIO.read(new File(path + "/cross.png")));
+            images.put("grass1",ImageIO.read(new File(path + "/grass1.png")));
+            images.put("grass2",ImageIO.read(new File(path + "/grass2.png")));
+            images.put("downStationHorizontalBlue",ImageIO.read(new File(path + "/downStationHorizontalBlue.png")));
+            images.put("downStationHorizontalGreen",ImageIO.read(new File(path + "/downStationHorizontalGreen.png")));
+            images.put("downStationHorizontalRed",ImageIO.read(new File(path + "/downStationHorizontalRed.png")));
+            images.put("downStationHorizontalYellow",ImageIO.read(new File(path + "/downStationHorizontalYellow.png")));
+            images.put("downStationVerticallBlue",ImageIO.read(new File(path + "/downStationVerticallBlue.png")));
+            images.put("downStationVerticalGreen",ImageIO.read(new File(path + "/downStationVerticalGreen.png")));
+            images.put("downStationVerticalRed",ImageIO.read(new File(path + "/downStationVerticalRed.png")));
+            images.put("downStationVerticalYellow",ImageIO.read(new File(path + "/downStationVerticalYellow.png")));
+            images.put("upStationHorizontalBlue",ImageIO.read(new File(path + "/upStationHorizontalBlue.png")));
+            images.put("upStationHorizontalGreen",ImageIO.read(new File(path + "/upStationHorizontalGreen.png")));
+            images.put("upStationHorizontalRed",ImageIO.read(new File(path + "/upStationHorizontalRed.png")));
+            images.put("upStationHorizontalYellow",ImageIO.read(new File(path + "/upStationHorizontalYellow.png")));
+            images.put("upStationVerticallBlue",ImageIO.read(new File(path + "/upStationVerticallBlue.png")));
+            images.put("upStationVerticalGreen",ImageIO.read(new File(path + "/upStationVerticalGreen.png")));
+            images.put("upStationVerticalRed",ImageIO.read(new File(path + "/upStationVerticalRed.png")));
+            images.put("upStationVerticalYellow",ImageIO.read(new File(path + "/upStationVerticalYellow.png")));
+            images.put("switchNorthRightCurve",ImageIO.read(new File(path + "/switchNorthRightCurve.png")));
+            images.put("switchNorthRightStraight",ImageIO.read(new File(path + "/switchNorthRightStraight.png")));
+            images.put("switchNorthLeftCurve",ImageIO.read(new File(path + "/switchNorthLeftCurve.png")));
+            images.put("switchNorthLeftStraight",ImageIO.read(new File(path + "/switchNorthLeftStraight.png")));
+            images.put("switchSouthRightCurve",ImageIO.read(new File(path + "/switchSouthRightCurve.png")));
+            images.put("switchSouthRightStraight",ImageIO.read(new File(path + "/switchSouthRightStraight.png")));
+            images.put("switchSouthLeftCurve",ImageIO.read(new File(path + "/switchSouthLeftCurve.png")));
+            images.put("switchSouthLeftStraight",ImageIO.read(new File(path + "/switchSouthLeftStraight.png")));
+            images.put("switchWestRightCurve",ImageIO.read(new File(path + "/switchWestRightCurve.png")));
+            images.put("switchWestRightStraight",ImageIO.read(new File(path + "/switchWestRightStraight.png")));
+            images.put("switchWestLeftCurve",ImageIO.read(new File(path + "/switchWestLeftCurve.png")));
+            images.put("switchWestLeftStraight",ImageIO.read(new File(path + "/switchWestLeftStraight.png")));
+            images.put("switchEastRightCurve",ImageIO.read(new File(path + "/switchEastRightCurve.png")));
+            images.put("switchEastRightStraight",ImageIO.read(new File(path + "/switchEastRightStraight.png")));
+            images.put("switchEastLeftCurve",ImageIO.read(new File(path + "/switchEastLeftCurve.png")));
+            images.put("switchEastLeftStraight",ImageIO.read(new File(path + "/switchEastLeftStraight.png")));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     static private Image getImage(String layer) {
-        String[] parts = layer.split(" ");
-        if (parts[0].equals())
-            return valto;
-        return valto;
+        if (images.containsKey(layer))
+            return images.get(layer);
+        return images.get(" ");
     }
 
 }
